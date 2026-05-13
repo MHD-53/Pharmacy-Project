7ce5f0aa301b6953757b2d67c5aee7feb5fcdea7
MHD-53 <mohammadcraftgame@gmail.com>
2026-05-13 15:27:19 +0300

    Add Invoice Class.

---
+++ Pharmacy-Project/Invoice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Project
+{
+    internal class Invoice
+    {
+        public List<Medicine> Medicines { get; set; }
+        public List<int> Quantities { get; set; }
+        public DateTime Date { get; set; }
+        public double Total { get; set; }
+
+        public Invoice()
+        {
+            Medicines = new List<Medicine>();
+            Quantities = new List<int>();
+            Date = DateTime.Now;
+            Total = 0;
+        }
+
+        public void CalculateTotal()
+        {
+            Total = 0;
+            for (int i = 0; i < Medicines.Count; i++)
+            {
+                Total += Medicines[i].Price * Quantities[i];
+            }
+        }
+    }
+}
